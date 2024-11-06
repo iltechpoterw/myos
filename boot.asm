a9b059bed2c1631c9c9a79d4a72c64416a8a0d7b
AndroMag <133264092+iltechpoterw@users.noreply.github.com>
2024-11-06 20:36:51 +0300

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,1 +1,13 @@
-boot.asm
+; boot.asm
+section .multiboot
+    magic:      dd 0x1BADB002
+    flags:      dd 0
+    checksum:   dd -(0x1BADB002 + 0)
+
+section .text
+global _start
+_start:
+    ; Устанавливаем стек и передаем управление ядру
+    call kernel_main
+    cli
+    hlt
